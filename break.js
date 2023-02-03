68010236f89ef8c8d172ad011cbb5c7696fcee37
sabbir1257 <masabbir1257@gmail.com>
2023-02-03 15:30:56 +0600

    push

---
+++ break.js
@@ -0,0 +1,17 @@
+// for(var i = 1; i <= 20; i++){
+//     console.log(i);
+//     if(i > 5){
+//         break;
+//     }
+// }
+
+
+
+var roastGiven = 0;
+while(roastGiven < 10) {
+    console.log('Roast dan');
+    roastGiven++;
+    if(roastGiven > 4) {
+        break;
+    }
+}
