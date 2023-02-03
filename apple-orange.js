63fb903b8e9792156a5802d6f4e5ce98aecd08e7
sabbir1257 <masabbir1257@gmail.com>
2023-02-03 22:52:50 +0600

    push

---
+++ apple-orange.js
@@ -0,0 +1,7 @@
+var moneyGive = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+var totalPrice = applePrice + orangePrice;
+console.log(totalPrice);
+var getBack = moneyGive - totalPrice;
+console.log(getBack);
